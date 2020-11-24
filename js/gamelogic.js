2fe33e381f6904c77687e382ed34cba728299be6
Dominic Chu <dominic.m.chu@gmail.com>
2020-11-24 18:11:52 -0500

    refocus back to basics

--- js/gamelogic.js
+++ js/gamelogic.js
@@ -1,58 +1,16 @@
 const _ = require('lodash')
-const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
-  });
 
-const prompts = require('prompts');
-
-const game = () => {
-    let numberOfPlayers = 0;
-    let deck = [];
+const game = (numberOfPlayers) => {
+    let deck = createRandomDeck();
     let cardIndex = 0;
-    let gameData = {};
+    let gameData = dealCards(numberOfPlayers, deck, cardIndex);
 
-    // start prompt: ask how many players, 
-    // assign deck 
-    // assign numberOfPlayers
-    readline.question("Hello, welcome to Holdem, how many players do you want to play against?", (answer) => {
-        console.log(`So you'll be playing against ${answer} players!`);
-        deck = createRandomDeck()
-        console.log("deck", deck) 
-        numberOfPlayers = (parseInt(answer) + 1);
-        console.log("numberOfPlayers", numberOfPlayers)
-        // cardIndex = numberOfPlayers * 2 + 5; 
-        // console.log("cardIndex in game", cardIndex)
-        console.log("Everyone starts with $100");
 
-        // console.log(dealCards(numberOfPlayers, deck))    
-        readline.question("Shall we play? (y/n) ", (answer) => {
-            if(answer === 'y') {
-                fullRound(numberOfPlayers, deck, gameData, cardIndex)
-            } else if (answer === 'n') {
-                console.log("Goodbye!")
-            }
-        })
-        
-    });
-
-    // convert everything to prompts + async await 
-
-    // start of round 
-    // check for cardIndex to make sure there are still shuffled cards in the deck if not, create new deck 
-    // check $ 
-    // pass out cards 
-    // tell player what they have 
-    // ask if player wants to bet pre-flop 
-    // show flop 
-    // ask if player wants to bet before turn 
-    // show turn 
-    // ask if player wants to bet before river 
-    // show river 
+    `You have a ${convertCards(gameData['Player 1']['card 1'])} and a ${convertCards(gameData['Player 1']['card 2'])}`
 
 }; 
 
-game()
+game(3) 
 
 const createRandomDeck = () => {
     const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
@@ -76,12 +34,6 @@ const createRandomDeck = () => {
 // createRandomDeck()
 
 const dealCards = (numberOfPlayers, deck, cardIndex) => {
-
-    // need to seperate logic to make it so that we can take in a cardIndex arg and start from there and not from index 0 
-    //
-    // ***********
-    // now need to check to make sure that this is working 
-
     const dealt = {}; 
     let currentDeckIndex = 0;
 
@@ -143,57 +95,3 @@ const convertCards = (cardObj) => {
 
     return cardString;
 };
-
-const fullRound = (numberOfPlayers, deck, gameData, cardIndex) => {
-    // console.log("fullRound Kicked")
-
-    // start of round 
-    // check for cardIndex to make sure there are still shuffled cards in the deck if not, create new deck 
-    // check $ 
-
-    // pass out cards 
-    gameData = {
-        ...gameData,
-        ...dealCards(numberOfPlayers, deck, cardIndex)
-    };
-    
-    // tell player what they have 
-    console.log(`You have a ${convertCards(gameData['Player 1']['card 1'])} and a ${convertCards(gameData['Player 1']['card 2'])}`)
-
-    // ask if player wants to bet pre-flop 
-    readline.question("Would you like to bet before the flop? (y/n) ", function preFlopBet(answer) {
-        if (answer === 'y') {
-
-        } else if (answer === 'n') {
-
-        } else {
-            preFlopBet()
-        }
-    })
-
-    // show flop 
-    // ask if player wants to bet before turn 
-    // show turn 
-    // ask if player wants to bet before river 
-    // show river 
-
-}
-
-// {
-//     Pool: {
-//       'card 1': { number: 12, suit: 2 },
-//       'card 2': { number: 13, suit: 1 },
-//       'card 3': { number: 9, suit: 3 },
-//       'card 4': { number: 11, suit: 4 },
-//       'card 5': { number: 12, suit: 1 }
-//     },
-//     'Player 1': { 'card 1': { number: 8, suit: 1 }, 'card 2': undefined },
-//     'Player 2': {
-//       'card 1': { number: 6, suit: 3 },
-//       'card 2': { number: 3, suit: 1 }
-//     },
-//     'Player 3': {
-//       'card 1': { number: 10, suit: 1 },
-//       'card 2': { number: 3, suit: 2 }
-//     }
-// }
