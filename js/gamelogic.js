bca32e0788afbff61c6f00feb1d2f7862fb8426d
Dominic Chu <dominic.m.chu@gmail.com>
2020-11-24 13:46:28 -0500

    some basic files

---
+++ js/gamelogic.js
@@ -0,0 +1,23 @@
+const _ = require('lodash')
+
+export default gamelogic = {
+    game,
+
+}
+
+function game() {
+    const gameData = {}; 
+
+}; 
+
+function createRandomDeck() {
+    const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+    // make helper to convert numbers to faces
+    const suitNumbers = [1, 2, 3, 4]
+    // make helper to convert suitNumbers to suits 4 as hearts etcetc
+
+}
+
+function convertNumbers() {}
+
+function convertSuits() {}
